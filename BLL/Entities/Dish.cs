70f05a2ea4d431100f2b39c5bbcec12cb4cf77a8
Ярослав Масенко <chasodij@outlook.com>
2023-02-04 18:18:42 +0200

    Add entities to DAL.

---
+++ BLL/Entities/Dish.cs
@@ -0,0 +1,15 @@
+﻿namespace chef.BLL.Entities
+{
+    public class Dish
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Portions { get; set; }
+        public int CookingTimeInMinutes { get; set; }
+        public int Weight { get; set; }
+        public string Recipe { get; set; }
+        public string MenuSection { get; set; }
+        public decimal Price { get; set; }
+        public string EnergyCategory { get; set; }
+    }
+}
---
+++ BLL/Entities/Ingredient.cs
@@ -0,0 +1,11 @@
+﻿namespace chef.BLL.Entities
+{
+    public class Ingredient
+    {
+        public int ProductTypeId { get; set; }
+        public string ProductTypeName { get; set; }
+        public int DishId { get; set; }
+        public string DishName { get; set; }
+        public double AmountInUnits { get; set; }
+    }
+}
