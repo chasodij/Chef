ea6f48f42f34608c7b60dd774e006160f43a0392
Ярослав Масенко <chasodij@outlook.com>
2023-02-05 17:07:52 +0200

    Add unit of work factory.

---
+++ DAL/UnitOfWork/AdoUnitOfWorkFactory.cs
@@ -0,0 +1,15 @@
+﻿using chef.BLL.UnitOfWork;
+using System.Data.SqlClient;
+
+namespace chef.DAL.UnitOfWork
+{
+    public class AdoUnitOfWorkFactory : IUnitOfWorkFactory
+    {
+        public IUnitOfWork Create()
+        {
+            var connectionString = "Data Source=DESKTOP-MM9MS8C\\SQLEXPRESS;Initial Catalog=chef_db;Integrated Security=True";
+            var con = new SqlConnection(connectionString);
+            return new AdoUnitOfWork(con);
+        }
+    }
+}
