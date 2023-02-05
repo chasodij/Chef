0623b0efae296bdfbadb615124cb09a4e41f14a8
Ярослав Масенко <chasodij@outlook.com>
2023-02-05 17:06:31 +0200

    Add extension for DbCommand.

---
+++ DAL/Extensions/DbCommandExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+
+namespace chef.DAL.Extensions
+{
+    public static class DbCommandExtensions
+    {
+        public static void AddParameterWithValue(this IDbCommand command, string name, object value)
+        {
+            var parameter = command.CreateParameter();
+            parameter.ParameterName = name;
+            parameter.Value = value;
+            command.Parameters.Add(parameter);
+        }
+    }
+}
