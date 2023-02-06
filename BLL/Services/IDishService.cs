3c37ec342ea7dbd07a503d0b77a5e7b828d88b46
Ярослав Масенко <chasodij@outlook.com>
2023-02-06 10:36:34 +0200

    Add dish service.

---
+++ BLL/Services/IDishService.cs
@@ -0,0 +1,14 @@
+﻿using chef.BLL.Entities;
+using System.Collections.Generic;
+
+namespace chef.BLL.Services
+{
+    public interface IDishService
+    {
+        void GetMaxValues(out int maxPortions, out int maxTime, out int maxWeight, out float maxPrice);
+        IEnumerable<Dish> GetDishesByName(string name);
+        Dish GetDishById(int id);
+        void DeleteDish(int id);
+        (bool canDelete, string error) CanDeleteDish(Dish dish);
+    }
+}
