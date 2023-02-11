d7e2a4c0f189f303310230cb1e0979a92d6b8e2e
Ярослав Масенко <chasodij@outlook.com>
2023-02-11 19:22:16 +0200

    Add property with Product Type to Ingredient entity.

--- DAL/Entities/Ingredient.cs
+++ DAL/Entities/Ingredient.cs
@@ -3,10 +3,9 @@
     public class Ingredient
     {
         public int ProductTypeId { get; set; }
-        public string ProductTypeName { get; set; }
         public int DishId { get; set; }
         public string DishName { get; set; }
         public double AmountInUnits { get; set; }
-        public string UnitsOfMeasurement { get; set; }
+        public ProductType ProductType { get; set; }
     }
 }
